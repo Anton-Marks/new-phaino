6b5afcc681f14c1aaf9e4d6de7a0bfdf52abbf70
Antonio Ortiz Avila <aortiz.muchaweb@gmail.com>
2024-05-23 23:31:46 -0600

    add basic translate

--- js/lang.js
+++ js/lang.js
@@ -1,13 +1,43 @@
-let translations = {};
-
 const baseWorld = 'phaino-'
-const nameDataOptionLang = 'data-default-i18n';
-const nameOptionLang = 'data-option-i18n';
+const baseWorldMovil = 'phaino-m-'
+const baseWorldTtlFig = 'phaino-ttl-'
 
-const optionLang = {
-    0: "esp",
-    1: "eng"
+const setTranslateMenu = (options) => {
+    Object.entries(options).forEach(([key, val]) => {
+        document.querySelector(`#${baseWorld}${key}`).textContent = val
+        document.querySelector(`#${baseWorldMovil}${key}`).textContent = val
+    });
 }
+
+const setTranslateSections = (options) => {
+    Object.entries(options).forEach(([key, val]) => {
+        document.querySelector(`#${baseWorld}${key}`).textContent = val
+    });
+}
+
+const setTranslateFigures = (options) => {
+    Object.entries(options).forEach(([key, val]) => {
+        document.querySelector(`#${baseWorld}${key}`).textContent = val
+        document.querySelector(`#${baseWorldTtlFig}${key}`).textContent = val
+    });
+}
+
+
+const translatePage = ( data ) => {
+
+    if (Object.keys(data.menu).length > 0) {
+        setTranslateMenu(data.menu)
+    }
+
+    if (Object.keys(data.menu).length > 0) {
+        setTranslateFigures(data.figures)
+    }
+
+    /*if (Object.keys(data.sections).length > 0) {
+        setTranslateSections(data.sections)
+    }*/
+
+};
 
 const fetchContent = async (optionLang) => {
     const response = await fetch(`lang/${optionLang}.json`);
@@ -20,48 +50,49 @@ const fetchContent = async (optionLang) => {
     
 };
 
-const setTranslateMenu = (options) => {
-
-    Object.entries(options).forEach(([key, val]) => {
-        document.querySelector(`#${baseWorld}${key}`).textContent = val
-    });
-    
+const getFetchLanguage = (el) => {
+    if (el) {
+        const acronimLang = el.getAttribute('data-i18n')
+        if (acronimLang) {
+            fetchContent(acronimLang);
+        }
+    }
 }
 
-const setTranslateSections = (options) => {
+const changeCurrentClassLanguage = (currentNumber) => {
+    const optionI18ns2 = currentNumber ===  0 ? 1 : 0;
 
-    Object.entries(options).forEach(([key, val]) => {
-        document.querySelector(`#${baseWorld}${key}`).textContent = val
-    });
+    optionsFirst = document.querySelectorAll(`[data-option-i18n="${currentNumber}"]`)
+    optionsSecond = document.querySelectorAll(`[data-option-i18n="${optionI18ns2}"]`)
+
+    if (optionsFirst.length > 0) {
+        optionsFirst.forEach(item => {
+            item.classList.toggle('i18n-language-current');
+        })
+    }
+
+    if (optionsSecond.length > 0) {
+        optionsSecond.forEach(item => {
+            item.classList.toggle('i18n-language-current');
+        })
+    }
 
 }
 
-const translatePage = ( data ) => {
+const i18ns = document.querySelectorAll('[data-i18n]');
 
-    if (Object.keys(data.menu).length > 0) {
-        setTranslateMenu(data.menu)
-    }
+if (i18ns.length > 0) {
+    i18ns.forEach(item => {
+        item.addEventListener('click', function handleClick(e) {
+            if (e.target.hasAttribute('data-i18n')) {
+                if (!e.target.classList.contains('i18n-language-current')) {
+                    const optionI18ns = parseInt(e.target.getAttribute('data-option-i18n'));
+                    changeCurrentClassLanguage(optionI18ns);
 
-    if (Object.keys(data.sections).length > 0) {
-        setTranslateSections(data.sections)
-    }
+                    getFetchLanguage(e.target);
+                }
+            }
+        });
+    });
+}
 
-};
-
-
-const switcher = document.querySelector('#localization-switcher-phaino');
-
-switcher.addEventListener('click', (e) => {
-
-    const option = parseInt(e.target.getAttribute(nameOptionLang))
-    
-    e.target.setAttribute(nameDataOptionLang, optionLang[option]);
-    e.target.setAttribute(nameOptionLang, option === 0 ? 1 : 0);
-
-    const acronimLang = e.target.getAttribute(nameDataOptionLang)
-
-    if (acronimLang) {
-        fetchContent(acronimLang);
-    }
-    
-});
